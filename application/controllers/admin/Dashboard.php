48131197b02677bd4820d8dada98c5404f94bdcc
uyok023 <uyok023@gmail.com>
2020-06-10 01:36:53 +0700

    edit logout

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -1,11 +1,18 @@
 <?php
 	defined('BASEPATH') OR exit('No direct script access allowed');
 	
-	
-
 
 	class Dashboard extends CI_Controller {
 	
+		public function __construct()
+  	{
+  		parent::__construct();
+  		if ($this->session->userdata('status') != 'login') {
+			redirect(base_url('login'));
+		}
+  	}
+
+
 		public function index()
 		{
 			$this->load->view('_partials/head');
--- application/views/_partials/navbar.php
+++ application/views/_partials/navbar.php
@@ -14,7 +14,7 @@
           <!-- Nav items -->
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" href="dashboard.html">
+              <a class="nav-link active" href="<?= base_url('admin/dashboard')  ?>">
                 <i class="ni ni-tv-2 text-primary"></i>
                 <span class="nav-link-text">Dashboard</span>
               </a>
