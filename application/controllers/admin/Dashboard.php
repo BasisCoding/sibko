29f50f928ffe4514749ad9ce05f180f60ba9e8b3
uyok023 <uyok023@gmail.com>
2020-06-09 22:28:25 +0700

    dashboard

---
+++ application/controllers/admin/Dashboard.php
@@ -0,0 +1,20 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	
+	class Dashboard extends CI_Controller {
+	
+		public function index()
+		{
+			$this->load->view('_partials/head');
+			$this->load->view('_partials/navbar');
+			$this->load->view('_partials/header');
+			$this->load->view('_partials/main');
+			$this->load->view('_partials/footer');
+			$this->load->view('_partials/plugin');
+			$this->load->view('services/admin/dashboard');
+		}	
+	}
+	
+	/* End of file Login.php */
+	/* Location: ./application/controllers/Login.php */
+ ?>
