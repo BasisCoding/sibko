b979010523efe9eb4b13f960278e905b2e45dd3f
unknown <basiscoding20@gmail.com>
2020-06-10 00:06:22 +0700

    Controller Login

--- database/sibko.sql
+++ database/sibko.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 09 Jun 2020 pada 14.43
+-- Waktu pembuatan: 09 Jun 2020 pada 19.05
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.6
 
@@ -29,13 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `user_aktif` (
   `id_user` int(11) NOT NULL,
-  `nama_user` int(11) NOT NULL,
-  `username` int(11) NOT NULL,
-  `email` int(11) NOT NULL,
-  `password` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  `foto` int(11) NOT NULL
+  `nama_user` varchar(150) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `status` varchar(15) NOT NULL,
+  `foto` varchar(150) NOT NULL,
+  `level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user_aktif`
+--
+
+INSERT INTO `user_aktif` (`id_user`, `nama_user`, `username`, `email`, `password`, `status`, `foto`, `level`) VALUES
+(1, 'Ahmad Fatoni', 'admin', 'achmad.fatoni33@gmail.com', '0ae02c00d2b1196589a5be37f718fbeec0c6f07968f90f41dbc2b167fdd919f57e9616c08130157ed4a22f7f7cae387276d456d01a98310f1b1d5f00999d5cb0', 'Aktif', '', 1);
 
 -- --------------------------------------------------------
 
@@ -46,8 +54,36 @@ CREATE TABLE `user_aktif` (
 CREATE TABLE `user_group` (
   `id` int(11) DEFAULT NULL,
   `nama_akses` varchar(150) NOT NULL,
-  `level` int(11) NOT NULL
+  `level` int(11) NOT NULL,
+  `link` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user_group`
+--
+
+INSERT INTO `user_group` (`id`, `nama_akses`, `level`, `link`) VALUES
+(NULL, 'Administrator', 1, 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `user_aktif`
+--
+ALTER TABLE `user_aktif`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `user_aktif`
+--
+ALTER TABLE `user_aktif`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
