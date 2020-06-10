ce28b77c0dd72f8ff12f443c522d10c639a09374
unknown <basiscoding20@gmail.com>
2020-06-10 17:51:34 +0700

    sidebar dinamis add

--- application/views/_partials/navbar.php
+++ application/views/_partials/navbar.php
@@ -13,94 +13,45 @@
         <div class="collapse navbar-collapse" id="sidenav-collapse-main">
           <!-- Nav items -->
           <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" href="<?= base_url('admin/dashboard')  ?>">
-                <i class="ni ni-tv-2 text-primary"></i>
-                <span class="nav-link-text">Dashboard</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="icons.html">
-                <i class="ni ni-single-02 text-yellow"></i>
-                <span class="nav-link-text">Biodata Siswa</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="map.html">
-                <i class="ni ni-pin-3 text-primary"></i>
-                <span class="nav-link-text">Orang Tua/Wali</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="profile.html">
-                <i class="ni ni-single-02 text-yellow"></i>
-                <span class="nav-link-text">Profile</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="tables.html">
-                <i class="ni ni-bullet-list-67 text-default"></i>
-                <span class="nav-link-text">Tables</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="login.html">
-                <i class="ni ni-key-25 text-info"></i>
-                <span class="nav-link-text">Login</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="register.html">
-                <i class="ni ni-circle-08 text-pink"></i>
-                <span class="nav-link-text">Register</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="upgrade.html">
-                <i class="ni ni-send text-dark"></i>
-                <span class="nav-link-text">Upgrade</span>
-              </a>
-            </li>
+            <?php
+              // data main menu
+                $main_menu = $this->db->get_where('menus', 
+                                          array('sub_menu' => 0, 'level' => $this->session->userdata('level')));
+                foreach ($main_menu->result() as $main) {
+                    // Query untuk mencari data sub menu
+                    $sub_menu = $this->db->get_where('menus', 
+                                          array('sub_menu' => $main->id, 'level' => $this->session->userdata('level')));
+                    // periksa apakah ada sub menu
+                  if ($sub_menu->num_rows() > 0) {?>
+                    <li class="nav-item">
+                      <a class="nav-link active" href="#">
+                        <i class=" text-primary"></i>
+                        <span class="nav-link-text">Dashboard</span>
+                      </a>
+                      <ul aria-expanded="false" class="collapse  first-level base-level-line">
+                          
+                          <?php foreach ($sub_menu->result() as $sub) {?>
+                            <li class="sidebar-item">
+                              <a href="<?= base_url($this->session->userdata('link')) ?>/<?= $sub->link ?>" class="sidebar-link">
+                                <span class="hide-menu"> <?= $sub->nama_menu ?></span>
+                              </a>
+                            </li>
+                          <?php } ?>
+                          
+                      </ul>
+                    </li>
+                  <?php } else { ?>
+                    <li class="nav-item">
+                      <a class="nav-link" href="<?= $main->link ?>">
+                        <i class="<?= $main->icon ?> text-primary"></i>
+                        <span class="nav-link-text"><?= $main->nama_menu ?></span>
+                      </a>
+                    </li> 
+                  <?php }
+                } ?>
+            
           </ul>
-          <!-- Divider -->
-          <hr class="my-3">
-          <!-- Heading -->
-          <h6 class="navbar-heading p-0 text-muted">
-            <span class="docs-normal">Documentation</span>
-          </h6>
-          <!-- Navigation -->
-          <ul class="navbar-nav mb-md-3">
-            <li class="nav-item">
-              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html" target="_blank">
-                <i class="ni ni-spaceship"></i>
-                <span class="nav-link-text">Getting started</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html" target="_blank">
-                <i class="ni ni-palette"></i>
-                <span class="nav-link-text">Foundation</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html" target="_blank">
-                <i class="ni ni-ui-04"></i>
-                <span class="nav-link-text">Components</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/plugins/charts.html" target="_blank">
-                <i class="ni ni-chart-pie-35"></i>
-                <span class="nav-link-text">Plugins</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active active-pro" href="upgrade.html">
-                <i class="ni ni-send text-dark"></i>
-                <span class="nav-link-text">Upgrade to PRO</span>
-              </a>
-            </li>
-          </ul>
+          
         </div>
       </div>
     </div>
