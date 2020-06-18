0b4cb335840d5312845e8ebd1b114d852715f9db
basiscoding20 <basiscoding20@gmail.com>
2020-06-18 07:54:53 -0700

    Logout Fix

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -6,8 +6,19 @@ class Logout extends CI_Controller {
 
 	public function index()
 	{
-		$this->session->sess_destroy();
-		redirect(base_url('login'));
+		if ($this->session->userdata('link') == 'admin') {
+			$this->session->sess_destroy();
+			redirect(base_url('admin/login'));
+		}
+		if ($this->session->userdata('link') == 'guru') {
+			$this->session->sess_destroy();
+			redirect(base_url('guru/login'));
+		}
+		if ($this->session->userdata('link') == 'ortu') {
+			$this->session->sess_destroy();
+			redirect(base_url('login'));
+		}
+
 	}
 
 }
